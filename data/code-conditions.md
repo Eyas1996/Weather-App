d0f11f0281316c3aeeddea1dc727a2a9bdad7f92
Eyas1996 <eyas.hm7@gmail.com>
2023-05-23 19:14:29 +0200

    Update Files

---
+++ data/code-conditions.md
@@ -0,0 +1,76 @@
+**Weather condition codes**
+
+**_Group 2xx: Thunderstorm_**
+ID Main Description Icon
+200 Thunderstorm thunderstorm with light rain 11d
+201 Thunderstorm thunderstorm with rain 11d
+202 Thunderstorm thunderstorm with heavy rain 11d
+210 Thunderstorm light thunderstorm 11d
+211 Thunderstorm thunderstorm 11d
+212 Thunderstorm heavy thunderstorm 11d
+221 Thunderstorm ragged thunderstorm 11d
+230 Thunderstorm thunderstorm with light drizzle 11d
+231 Thunderstorm thunderstorm with drizzle 11d
+232 Thunderstorm thunderstorm with heavy drizzle 11d
+
+**_Group 3xx: Drizzle_**
+ID Main Description Icon
+300 Drizzle light intensity drizzle 09d
+301 Drizzle drizzle 09d
+302 Drizzle heavy intensity drizzle 09d
+310 Drizzle light intensity drizzle rain 09d
+311 Drizzle drizzle rain 09d
+312 Drizzle heavy intensity drizzle rain 09d
+313 Drizzle shower rain and drizzle 09d
+314 Drizzle heavy shower rain and drizzle 09d
+321 Drizzle shower drizzle 09d
+
+**_Group 5xx: Rain_**
+ID Main Description Icon
+500 Rain light rain 10d
+501 Rain moderate rain 10d
+502 Rain heavy intensity rain 10d
+503 Rain very heavy rain 10d
+504 Rain extreme rain 10d
+511 Rain freezing rain 13d
+520 Rain light intensity shower rain 09d
+521 Rain shower rain 09d
+522 Rain heavy intensity shower rain 09d
+531 Rain ragged shower rain 09d
+
+**_Group 6xx: Snow_**
+ID Main Description Icon
+600 Snow light snow 13d
+601 Snow snow 13d
+602 Snow heavy snow 13d
+611 Snow sleet 13d
+612 Snow light shower sleet 13d
+613 Snow shower sleet 13d
+615 Snow light rain and snow 13d
+616 Snow rain and snow 13d
+620 Snow light shower snow 13d
+621 Snow shower snow 13d
+622 Snow heavy shower snow 13d
+
+**_Group 7xx: Atmosphere_**
+ID Main Description Icon
+701 Mist mist 50d
+711 Smoke smoke 50d
+721 Haze haze 50d
+731 Dust sand/dust whirls 50d
+741 Fog fog 50d
+751 Sand sand 50d
+761 Dust dust 50d
+762 Ash volcanic ash 50d
+771 Squall squalls 50d
+781 Tornado tornado 50d
+
+**_Group 800: Clear_**
+800 Clear clear sky
+
+**_Group 80x: Clouds_**
+ID Main Description
+801 Clouds few clouds: 11-25%
+802 Clouds scattered clouds: 25-50%
+803 Clouds broken clouds: 51-84%
+804 Clouds overcast clouds: 85-100%
--- main.js
+++ main.js
@@ -1,157 +1,199 @@
 // ----------------------------------------------------------------
 // Get all Necessery Element from Dom
-let app = document.getElementById('app');
-let search = document.getElementById('search');
-let btn = document.getElementById('submit');
-let popCities = document.getElementById('popular-cities');
-let daily = document.getElementById('daily');
+let app = document.getElementById("app");
+let search = document.getElementById("search");
+let btn = document.getElementById("submit");
+let popCities = document.getElementById("popular-cities");
+let daily = document.getElementById("daily");
 // ----------------------------------------------------------------
 // Default City
-let cityInput = 'Berlin';
+let cityInput = "Berlin";
 // Add addEventListener to the Search btn
-btn.addEventListener('click', () => {
-	// // prevent Browser Default Reload
-	// event.preventDefault();
-	// change the city after typing it in the input field and click the btn
-	if (search.value.length < 4) {
-		alert('invalid Input');
-	} else {
-		cityInput = search.value;
-		fetchWeatherApi();
-	}
+btn.addEventListener("click", () => {
+  // // prevent Browser Default Reload
+  // event.preventDefault();
+  // change the city after typing it in the input field and click the btn
+  if (search.value.length < 3) {
+    alert("invalid Input");
+  } else {
+    cityInput = search.value;
+    fetchWeatherApi();
+  }
 });
 // ----------------------------------------------------------------
 // we Select a city form the Popular Cities List and add it the Api(Query String).
 for (let i = 0; i < popCities.children.length; i++) {
-	popCities.children[i].addEventListener('click', () => {
-		cityInput = event.target.textContent;
-		fetchWeatherApi();
-	});
+  popCities.children[i].addEventListener("click", (event) => {
+    cityInput = event.target.textContent;
+    fetchWeatherApi();
+  });
 }
 // ----------------------------------------------------------------
 // Main Function That fetches and display all Wanted data form the Weather API
 async function fetchWeatherApi() {
-	let resopse = await fetch('./data/weather.json');
-	// let resopse = await fetch(
-	// 	`https://api.weatherapi.com/v1/forecast.json?key=f1a7e2ed4cc8463a884192227223011&q=${cityInput}&days=10&aqi=no&alerts=no`
-	// );
-	let data = await resopse.json();
+  let respons = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&units=metric&appid=44f47ce10e9f96180cf1dcf529849f54`
+  );
+  // let respons = await fetch('./data/weather02.json');
+  let data = await respons.json();
 
-	console.log(data);
+  console.log(data);
 
-	// ----------------------------------------------------------------
-	// saving all wanted data form the Api into an Object
-	let dataObject = {
-		currentWeahter: `${data.current.temp_c}°`,
-		currentC: data.location.name,
-		currentTime: data.location.localtime,
-		currentMinMax: `${data.forecast.forecastday[0].day.mintemp_c}°/${data.forecast.forecastday[0].day.maxtemp_c}°`,
-		currentSkyIcon: data.current.condition.icon,
-		currentSkyStatus: data.current.condition.text,
-		skyInfo: `${data.current.cloud}%`,
-		feelsLike: `${data.current.feelslike_c}°`,
-		humidityInfo: `${data.current.humidity}%`,
-		windInfo: `${data.current.wind_kph}km/h`,
-		rainInfo: `${data.forecast.forecastday[0].day.daily_chance_of_rain}%`
-	};
-	console.log(dataObject);
-	// ----------------------------------------------------------------
-	// give Danamic Value to the HTML Elements from Api Data
-	document.getElementById('current-temp').textContent = dataObject.currentWeahter;
-	document.getElementById('current-city').textContent = dataObject.currentC;
-	document.getElementById('time').textContent = dataObject.currentTime;
-	document.getElementById('min-max-temp').textContent = dataObject.currentMinMax;
-	document.getElementById('icon').src = dataObject.currentSkyIcon;
-	document.getElementById('status').textContent = dataObject.currentSkyStatus;
-	document.getElementById('sky').textContent = dataObject.skyInfo;
-	document.getElementById('feel').textContent = dataObject.feelsLike;
-	document.getElementById('humidity').textContent = dataObject.humidityInfo;
-	document.getElementById('wind').textContent = dataObject.windInfo;
-	document.getElementById('rain').textContent = dataObject.rainInfo;
+  // ----------------------------------------------------------------
+  // saving all wanted data form the Api into an Object
+  let dataObject = {
+    currentWeahter: `${Math.trunc(data.main.temp)}°`,
+    currentC: data.name,
+    // currentTime: data.location.localtime,
+    currentMinMax: `${Math.trunc(data.main.temp_min)}°/${Math.trunc(
+      data.main.temp_max
+    )}°`,
+    currentSkyIcon: data.weather[0].icon,
+    currentSkyStatus: data.weather[0].main,
+    feelsLike: `${Math.trunc(data.main.feels_like)}°`,
+    // skyInfo: `${data.weather[0].main}`,
+    description: `${data.weather[0].description}`,
+    humidityInfo: `${data.main.humidity}%`,
+    windInfo: `${data.wind.speed}km/h`,
+  };
+  console.log(dataObject.windInfo);
+  // ----------------------------------------------------------------
+  // give Danamic Value to the HTML Elements from Api Data
+  document.getElementById("current-temp").textContent =
+    dataObject.currentWeahter;
+  document.getElementById("current-city").textContent = dataObject.currentC;
+  // document.getElementById("time").textContent = dataObject.currentTime;
+  document.getElementById("min-max-temp").textContent =
+    dataObject.currentMinMax;
 
-	// ----------------------------------------------------------------
-	// function to Display Days
-	const displayDaysForecast = () => {
-		daily.textContent = '';
-		for (let i = 0; i < data.forecast.forecastday.length; i++) {
-			let theDays = [ 'So', 'Mo', 'Tu', 'We', 'Thu', 'Fr', 'Sa' ];
-			let date = new Date(data.forecast.forecastday[i].date);
-			let dayName = '';
-			if (i === 0) {
-				dayName = 'Today';
-			} else {
-				dayName = theDays[date.getDay()];
-			}
-			let day = document.createElement('div');
-			day.className = 'day';
-			day.innerHTML = `
-          <span>${dayName}</span>
-          <span><img src=${data.forecast.forecastday[i].day.condition.icon}></span>
-          <span>${data.forecast.forecastday[i].day.mintemp_c}°/${data.forecast.forecastday[i].day.maxtemp_c}°</span>
-          <span>${data.forecast.forecastday[i].day.maxwind_kph}km/h</span>
-        `;
-			daily.appendChild(day);
-		}
-	};
-	displayDaysForecast();
-	// ----------------------------------------------------------------
-	// function to change the backgroundImage
-	const changeBg = () => {
-		// Set default time of day
-		let timeOfDay = 'Day';
-		// Get the unique id-code for each weather condition
-		let code = data.current.condition.code;
-		// Change it to night if its night time in the selected City
-		if (!data.current.is_day) {
-			timeOfDay = 'Night';
-		}
-		if (code === 1000) {
-			// Set the background Image to clear if the Weather is clear
-			app.style.backgroundImage = `url(../Images/${timeOfDay}/clear.jpg)`;
-		} else if (
-			code === 1003 ||
-			code === 1006 ||
-			code === 1009 ||
-			code === 1030 ||
-			code === 1087 ||
-			code === 1135 ||
-			code === 1273 ||
-			code === 1276 ||
-			code === 1279 ||
-			code === 1282
-		) {
-			//if its Cloudy Weather
-			app.style.backgroundImage = `url(../Images/${timeOfDay}/cloudy.jpg)`;
-		} else if (
-			code === 1063 ||
-			code === 1069 ||
-			code === 1072 ||
-			code === 1150 ||
-			code === 1153 ||
-			code === 1180 ||
-			code === 1183 ||
-			code === 1186 ||
-			code === 1189 ||
-			code === 1192 ||
-			code === 1195 ||
-			code === 1204 ||
-			code === 1207 ||
-			code === 1240 ||
-			code === 1243 ||
-			code === 1243 ||
-			code === 1246 ||
-			code === 1249 ||
-			code === 1252
-		) {
-			//if its Raining Weather
-			app.style.backgroundImage = `url(../Images/${timeOfDay}/rain.jpg)`;
-		} else {
-			// for Snow Weather
-			app.style.backgroundImage = `url(../Images/${timeOfDay}/snow.jpg)`;
-		}
-		app.style.opacity = '1';
-	};
-	changeBg();
+  document.getElementById(
+    "icon"
+  ).src = `https://openweathermap.org/img/wn/${dataObject.currentSkyIcon}@2x.png`;
+  document.getElementById("status").textContent = dataObject.currentSkyStatus;
+  document.getElementById("feel").textContent = dataObject.feelsLike;
+  document.getElementById("description").textContent = dataObject.description;
+  document.getElementById("humidity").textContent = dataObject.humidityInfo;
+  document.getElementById("wind").textContent = dataObject.windInfo;
+
+  // ----------------------------------------------------------------
+  // function to Display Days
+  // const displayDaysForecast = () => {
+  // 	daily.textContent = '';
+  // 	for (let i = 0; i < data.forecast.forecastday.length; i++) {
+  // 		let theDays = [ 'So', 'Mo', 'Tu', 'We', 'Thu', 'Fr', 'Sa' ];
+  // 		let date = new Date(data.forecast.forecastday[i].date);
+  // 		let dayName = '';
+  // 		if (i === 0) {
+  // 			dayName = 'Today';
+  // 		} else {
+  // 			dayName = theDays[date.getDay()];
+  // 		}
+  // 		let day = document.createElement('div');
+  // 		day.className = 'day';
+  // 		day.innerHTML = `
+  //         <span>${dayName}</span>
+  //         <span><img src=${data.forecast.forecastday[i].day.condition.icon}></span>
+  //         <span>${data.forecast.forecastday[i].day.mintemp_c}°/${data.forecast.forecastday[i].day.maxtemp_c}°</span>
+  //         <span>${data.forecast.forecastday[i].day.maxwind_kph}km/h</span>
+  //       `;
+  // 		daily.appendChild(day);
+  // 	}
+  // };
+  // displayDaysForecast();
+  // ----------------------------------------------------------------
+  // function to change the backgroundImage
+  const changeBg = () => {
+    // Set default time of day
+    let timeOfDay = "Night";
+    // give a random Day OR Night
+    let num = Math.floor(Math.random() * 2) + 1;
+    if (num === 1) {
+      timeOfDay = "day";
+    }
+    console.log(timeOfDay);
+    // Get the unique id-code for each weather condition
+    let code = data.weather[0].id;
+    // Set the background Image to clear if the Weather is Thunderstorm
+    if (
+      code === 200 ||
+      code === 201 ||
+      code === 202 ||
+      code === 210 ||
+      code === 211 ||
+      code === 212 ||
+      code === 230 ||
+      code === 231 ||
+      code === 232
+    ) {
+      app.style.backgroundImage = `url(../Images/${timeOfDay}/thunder.jpg)`;
+    } else if (
+      // for Drizzle Weather
+      code === 300 ||
+      code === 301 ||
+      code === 302 ||
+      code === 310 ||
+      code === 311 ||
+      code === 312 ||
+      code === 313 ||
+      code === 314 ||
+      code === 321
+    ) {
+      app.style.backgroundImage = `url(../Images/${timeOfDay}/rain.jpg)`;
+    } else if (
+      // for Rain Weather
+      code === 500 ||
+      code === 501 ||
+      code === 502 ||
+      code === 503 ||
+      code === 504 ||
+      code === 511 ||
+      code === 520 ||
+      code === 521 ||
+      code === 522 ||
+      code === 531
+    ) {
+      app.style.backgroundImage = `url(../Images/${timeOfDay}/rain.jpg)`;
+    } else if (
+      // for Snow
+      code === 600 ||
+      code === 601 ||
+      code === 602 ||
+      code === 611 ||
+      code === 612 ||
+      code === 613 ||
+      code === 615 ||
+      code === 616 ||
+      code === 620 ||
+      code === 621 ||
+      code === 622
+    ) {
+      app.style.backgroundImage = `url(../Images/${timeOfDay}/snow.jpg)`;
+    } else if (
+      // for Atmosphere
+      code === 600 ||
+      code === 601 ||
+      code === 602 ||
+      code === 611 ||
+      code === 612 ||
+      code === 613 ||
+      code === 615 ||
+      code === 616 ||
+      code === 620 ||
+      code === 621 ||
+      code === 622
+    ) {
+      app.style.backgroundImage = `url(../Images/${timeOfDay}/fogy.jpg)`;
+    } else if (code === 800) {
+      // for Clear Weather
+      app.style.backgroundImage = `url(../Images/${timeOfDay}/clear.jpg)`;
+    } else if (code === 801 || code === 802 || code === 803 || code === 804) {
+      //if its Cloudy Weather
+      app.style.backgroundImage = `url(../Images/${timeOfDay}/cloudy.jpg)`;
+      //if its Raining Weather
+    }
+    app.style.opacity = "1";
+  };
+  changeBg();
 }
 
 fetchWeatherApi();
